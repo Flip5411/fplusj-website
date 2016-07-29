63966682aaa6d9c65f14f27d9a1c9d752ccb2123
Felipe Jardim <flipmode011@gmail.com>
2016-07-28 18:46:13 -0700

    testing new push after heroku rename

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, function () {
-	console.log("Express server running on: " + PORT);
+	console.log("Express server running on: " + PORT +" ...exciting stuff...");
 });
