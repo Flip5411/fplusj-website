a899f5bb46b0c6afc5d1bcb21fc728074788f5db
Felipe Jardim <flipmode011@gmail.com>
2016-07-28 21:23:01 -0700

    updated server.js to log root dir

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
+console.log("Root directory is: " + __dirname + "/public");
 
 app.listen(PORT, function () {
 	console.log("Express server running on: " + PORT +" ...exciting stuff...");
