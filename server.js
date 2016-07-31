92bb8436366680babe695e93b68265ff21984f49
Felipe Jardim <flipmode011@gmail.com>
2016-07-30 17:15:33 -0700

    first design and css layout

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public"));
 console.log("Root directory is: " + __dirname + "/public");
+
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(PORT, function () {
 	console.log("Express server running on: " + PORT +" ...exciting stuff...");
