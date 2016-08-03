4bc22562e31e0b90863a85e10d34dacd0f9e89b9
Felipe Jardim <flipmode011@gmail.com>
2016-08-03 09:51:56 -0700

    added form and favicon, resized img

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,18 @@ window.onload = function () {
 
 	console.log("app.js hit");
 
+	$("#form-US-btn").click(function () {
+		$(this).addClass("btn-active");
+		$("#form-notUS-btn").removeClass("btn-active");
+		$("#international-address").hide();
+		$("#US-address").show();
+	});
+
+	$("#form-notUS-btn").click(function () {
+		$(this).addClass("btn-active");
+		$("#form-US-btn").removeClass("btn-active");
+		$("#US-address").hide();
+		$("#international-address").show();
+	});
+
 }
