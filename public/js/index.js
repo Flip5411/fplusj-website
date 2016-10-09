80db948974052abd11a69c6a79f210d4ea1bf1a8
Felipe Jardim <flipmode011@gmail.com>
2016-10-08 19:28:48 -0700

    fade ins and countdown

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,66 @@
 $(document).ready(function() {
 
-	// variables
+// ---- VARIABLES ----
+	// positions
 	var navHeight = 50;
 	var venuePosition;
 	var aboutPosition;
 	var navTrigger;
+	var elemBottom;
+	var windowBottom;
+
+	// countdown
+	var timeLeft;
+
+	// forms
+	var userFeedback = $("#user-feedback");
+	var sendBtn = $("#send-btn");
+
+// --- FUNCTIONS ----
+
+	// CLOCK COUNTDOWN
+	// countdown
+	function countdown() {
+		var now = Date.now();
+		var endDate = new Date("May 20 2017 18:30:00 PDT").getTime();
+		var milliseconds = endDate - now;
+		timeLeft = {
+			seconds: Math.floor((milliseconds / 1000) % 60),
+			minutes: Math.floor((milliseconds / 1000 / 60) % 60),
+			hours: Math.floor((milliseconds / 1000 / 60 / 60) % 24),
+			days: Math.floor(milliseconds / 1000 / 60 / 60 / 24)
+		}
+
+		// display each number in it's approprite place
+		// update the numbers every second
+	}
+
+	// FADE IN PARALLAX EFFECT
+	function fadeInContent() {
+		// text fade in
+		$(".fade-in-text").each(function() {
+			elemBottom = $(this).offset().top + $(this).outerHeight() - 75;
+			windowBottom = $(window).scrollTop() + $(window).height();
+			if (elemBottom < windowBottom) {
+				$(this).animate({"opacity": "1", bottom: "0"}, 750, "swing");
+			}
+		});
+
+		// blocks fade in
+		$(".fade-in-block").each(function() {
+			elemBottom = $(this).offset().top + $(this).outerHeight()/2 - 75;
+			windowBottom = $(window).scrollTop() + $(window).height();
+			if (elemBottom < windowBottom) {
+				$(this).animate({"opacity": "1", bottom: "0"}, 750, "swing");
+			}
+		});
+
+		// change fade behavior of certain elements for mobile
+		if ($(window).width() < 768) {
+			$("#extra .row").removeClass("fade-in-block");
+			$(".extra-content-area").addClass("fade-in-block");
+		}
+	}
 
 	// NAV CONTROL
 	function toggleNav() {
@@ -22,7 +78,7 @@ $(document).ready(function() {
 	}
 
 	// FIXED BACKGROUND CONTROL
-	// defining the right pics:
+		// defining the right pics:
 	function programBgPic() {
 		if (window.innerWidth > 768) {
 			return {
@@ -51,7 +107,7 @@ $(document).ready(function() {
 		}
 	}
 
-	// replacing the background pics
+		// replacing the background pics
 	function replaceFixedBg() {
 		venuePosition = $("#venue").offset().top;
 		aboutPosition = $("#about").offset().top;
@@ -65,7 +121,81 @@ $(document).ready(function() {
 		}
 	}
 
-	// EVENTS
+	// CONTACT FORM HANDLING
+	function sendForm () {
+		// validate form
+		function validateForm() {
+			var formArray = $(".form-control");
+			for (var i = 0; i < formArray.length; i++) {
+				if (formArray[i].checkValidity() === false) {
+					return false;
+				}
+			}
+			return true;
+		}
+		var formIsValid = validateForm();
+
+		// validation success
+		function acceptForm() {
+			// create email object
+			var email = {
+				name: $("#form-name").val(),
+				sender: $("#form-sender").val(),
+				subject: $("#form-subject").val(),
+				message: $("#form-message").val()
+			}
+
+			// tell user sending in progress
+			userFeedback.html("");
+			userFeedback.removeClass("negative");
+			userFeedback.addClass("positive");
+			userFeedback.css("display", "block");
+			userFeedback.html("Sending email, please wait...");
+
+			// send email
+			$.post("sendEmail", email, function(data) {
+				if (data === "error") {
+					// error message
+					userFeedback.removeClass("positive");
+					userFeedback.addClass("negative");
+					userFeedback.html("Hmmm, something messed up...oh well, just email us directly at <a href='mailto:FplusJ.2017@gmail.com'>FplusJ.2017@gmail.com</a>.");
+					console.log("Got an error back from server!");
+					sendBtn.addClass("disabled");
+					sendBtn.disabled = true;
+				} else {
+					// success message
+					userFeedback.html("Thanks, we got your email, " + data.name + "...we'll get back to you ASAP :)");
+					console.log("Server got the email info!");
+					sendBtn.addClass("disabled");
+					sendBtn.disabled = true;
+				}
+			});
+		}
+
+		// validation fail
+		function rejectForm() {
+			userFeedback.html("");
+			userFeedback.removeClass("positive");
+			userFeedback.addClass("negative");
+			setTimeout(function() {
+				userFeedback.html("Uh oh, you didn't fill out the form properly...it's ok, forms are hard...");
+				userFeedback.css("display", "block");
+			}, 100);
+			console.log("Invalid form");
+		}
+
+		// run the proper function
+		if (formIsValid) {
+			console.log("Form is valid!");
+			acceptForm();
+		} else {
+			console.log("Form is invalid!");
+			rejectForm();
+		}
+
+	}
+
+// ---- EVENTS ---- 
 	// nav click scroll control
 	$("#home a").click(function() {
 		var linkDest = $(this).attr("href");
@@ -75,19 +205,30 @@ $(document).ready(function() {
 		}, 1000);
 	});	
 
-	// set nav and backgrounds on load
+	// set nav, backgrounds, and fade-ins on load
 	toggleNav();
 	replaceFixedBg();
+	setTimeout(function() {fadeInContent()}, 1000);
+	setInterval(function() {
+		countdown();
+		$(".countdown .days").html(timeLeft.days);
+		$(".countdown .hours").html(timeLeft.hours);
+		$(".countdown .minutes").html(timeLeft.minutes);
+		$(".countdown .seconds").html(timeLeft.seconds);
+	}, 1000);
 
-	// set nav and backgrounds on scroll
+	// set nav, backgrounds, and fade-ins on scroll
 	$(document).scroll(function() {
 		toggleNav();
 		replaceFixedBg();
+		fadeInContent();
 	});
 
-	// set background images on screen resize
+	// set nav, backgrounds, and fade-ins on screen resize
 	$(window).resize(function() {
+		toggleNav();
 		replaceFixedBg();
+		fadeInContent();
 	});
 
 	// toggle between stat and bio cards
@@ -106,4 +247,18 @@ $(document).ready(function() {
 		$("#creamy-card .bio").slideToggle();		
 		$("#creamy-card .stats").slideToggle();
 	});
+
+	// send email from form
+	sendBtn.click(function() {
+		userFeedback.html("");
+		userFeedback.removeClass("negative");
+		userFeedback.removeClass("positive");
+		sendForm();
+	});
+
 });
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,9 +2,60 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
+	//email SMTP
+var nodemailer = require("nodemailer");
+// var smtpTransport = nodemailer.createTransport({
+// 	service: "Gmail",
+// 	auth: {
+// 		user: "fplusj.2017@gmail.com",
+// 		pass: "tresleches1101"
+// 	}
+// });
+
+//Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // serve webpage files to app
 app.use("/", express.static(__dirname + "/public"));
+
+// grab content from contact form and send email
+app.post("/sendEmail", function(req, res) {
+	var email = req.body;
+	// testing if i got the right info from client
+	console.log("Server got: " + email);
+	
+	var mailData = {
+		to: "FplusJ2017@gmail.com",
+		replyTo: email.sender,
+		subject: email.subject,
+		message: "Message from: " + email.name +
+			"\n\n" + email.message
+	};
+	// testing if i created the mailData properly in server
+	console.log(
+		"to: " + mailData.to +
+		"\nreply to: " + mailData.replyTo +
+		"\nsubject: " + mailData.subject +
+		"\nmessage: " + mailData.message
+	);
+
+	// test sending data back to client
+	// res.send(email);
+
+	// smtpTransport.sendMail(mailData, function(error, smtpRes) {
+	// 	console.log("code in sendMail hit:");
+	// 	console.log(mailData);
+	// 	if (error) {
+	// 		console.log("Error trying to send email");
+	// 		res.send("error");
+	// 	} else {
+	// 		console.log("Email sent!");
+	// 		res.send(email);
+	// 	}
+	// });
+});
 
 // Redirects
 app.get('/*', function(req, res){
@@ -13,5 +64,5 @@ app.get('/*', function(req, res){
 
 // run server from port
 app.listen(PORT, function () {
-	console.log("Express server running on: " + PORT +" ...fplusj is up...");
+	console.log("Express server running on: " + PORT + " ...fplusj is up...");
 });
