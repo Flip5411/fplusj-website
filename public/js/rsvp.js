add4f2d5f1ecb6efa4bffb4cbb9af2a261323f25
Felipe Jardim <flipmode011@gmail.com>
2017-02-07 19:59:22 -0800

    fixed entree vals to parse strings to integers

--- public/js/rsvp.js
+++ public/js/rsvp.js
@@ -24,9 +24,9 @@ $(document).ready(function() {
         } else {
             rsvp.attending = undefined;
         }
-        rsvp.beef = formBeef.val();
-        rsvp.fish = formFish.val();
-        rsvp.pasta = formPasta.val();
+        rsvp.beef = parseInt(formBeef.val());
+        rsvp.fish = parseInt(formFish.val());
+        rsvp.pasta = parseInt(formPasta.val());
         rsvp.song = formSong.val();
         console.log(rsvp);
     }
@@ -109,7 +109,7 @@ $(document).ready(function() {
                     // error message
                     userFeedback.removeClass('positive');
                     userFeedback.html("Hmmm, something messed up...oh well, just email us directly at <a href='mailto:FplusJ.2017@gmail.com'>FplusJ.2017@gmail.com</a>.");
-                    console.log("Got an error back from server!", serverData);
+                    console.log("Got an error back from server!");
                 } else {
                     // success message
                     userFeedback.addClass('positive');
@@ -118,7 +118,7 @@ $(document).ready(function() {
                     } else {
                         userFeedback.html("Awww, too bad you can't make it " + serverData.name.substr(0, serverData.name.indexOf(' ')) + ". That's ok though, let's keep in touch :)");
                     }
-                    console.log("Server got the rsvp info!", serverData);
+                    console.log("Server got the rsvp info!");
                 }
             });
 
